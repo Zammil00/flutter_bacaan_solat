97a2ff668be780a9bd509ceaea929f4776448d48
Zammil00 <muzammilkw07@gmail.com>
2025-03-17 03:33:39 +0700

    Start Code

---
+++ android/app/src/main/kotlin/com/example/bacaan_solat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bacaan_solat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LabilApp());
+}
+
+class LabilApp extends StatelessWidget {
+  const LabilApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
